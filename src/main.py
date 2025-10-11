dd2386c1f5d81bd49d5807f348f7da11f7841201
timmi <timmid999@gmail.com>
2025-10-11 13:42:02 -0700

    Prompting user for zip file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
+from parsing import parse_zip_file
+
 def main():
     print("This is the main flow of the system!")
+
+    zip_path = get_zip_path_from_user()
+    print(f"Recieved path: {zip_path}")
+    parse_zip_file(zip_path)
+
+def get_zip_path_from_user():
+    path = input("Please enter the path to your ZIP file: ").strip()
+    return path
 
 if __name__ == "__main__":
     main()
---
+++ src/parsing.py
@@ -0,0 +1,6 @@
+import os
+import zipfile
+import time
+
+def parse_zip_file(zip_path):
+    print(f"Parsing file: {zip_path}")
