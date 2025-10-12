6635274f9687c8b0f8190ee779395774d5e4cbf1
timmi <timmid999@gmail.com>
2025-10-12 12:18:51 -0700

    All data should be saved to the same spot every time

--- src/parsing.py
+++ src/parsing.py
@@ -3,6 +3,13 @@ import zipfile
 import time
 import json
 
+CURR_DIR = os.path.dirname(os.path.abspath(__file__)) # Gives the location of the script itself, not where user is running the command from
+REPO_ROOT = os.path.abspath(os.path.join(CURR_DIR, "..")) # Moves up one level into main repository directory
+
+ZIP_DATA_DIR = os.path.join(REPO_ROOT, "zip_data")
+RAWDATA_DIR = os.path.join(ZIP_DATA_DIR, "parsed_zip_rawdata")
+METADATA_PATH = os.path.join(ZIP_DATA_DIR, "parsed_zip_metadata.json")
+
 def parse_zip_file(zip_path):
     zip_path = str(zip_path)
 
@@ -18,26 +25,23 @@ def parse_zip_file(zip_path):
         print(f"Error: The file is not a valid ZIP archive.")
         return
     
+    # Make sure directories exist
+    os.makedirs(RAWDATA_DIR, exist_ok=True)
+    os.makedirs(os.path.dirname(METADATA_PATH), exist_ok=True)
+    
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         if len(zip_ref.namelist()) == 0:
             return []
 
-        extract_to = "../zip_data/parsed_zip_rawdata"
-        zip_ref.extractall(extract_to)
+        zip_ref.extractall(RAWDATA_DIR)
 
-    files_info = collect_file_info(extract_to)
+    files_info = collect_file_info(RAWDATA_DIR)
 
-    # Write to JSON file
-    output_path = "../zip_data/parsed_zip_metadata.json"
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    with open(output_path, "w", encoding="utf-8") as f:
+    with open(METADATA_PATH, "w", encoding="utf-8") as f:
         json.dump(files_info, f, indent=4)
 
-    print (f"Parsed file info has been saved to {output_path}")
-    #print("Extracted file info:")
-    #for f in files_info:
-        #print(f)
+    print (f"Extracted ZIP to: {RAWDATA_DIR}")
+    print(f"Metdata has been saved to: {METADATA_PATH}")
 
     return files_info
 
