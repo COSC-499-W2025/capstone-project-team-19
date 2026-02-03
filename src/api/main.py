def6c011c7c50c15d4eba737dbd5d4a766fc40e8
Timmi Draper <timmid999@gmail.com>
2026-02-03 13:04:50 -0800

    routes

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.api.routes import (
     projects_router,
     projects_ranking_router,
+    project_dates_router,
     skills_router,
     resumes_router,
     github_router,
@@ -19,6 +20,7 @@ def health():
 app.include_router(auth_router)
 app.include_router(projects_ranking_router)
 app.include_router(projects_router)
+app.include_router(project_dates_router)
 app.include_router(skills_router)
 app.include_router(resumes_router)
 app.include_router(consent_router)
