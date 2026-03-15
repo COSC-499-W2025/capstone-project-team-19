ed20756ad19a3adfb77f7c49abc4a8776b424872
AdaraPutri <adarasharmilaputri@gmail.com>
2026-03-15 15:04:22 -0700

    fixed tests

--- frontend/src/components/layout/PageContainer.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-export default function PageContainer({ children, className = "" }: Props) {
-  return <main className={`pageContainer ${className}`}>{children}</main>;
-}
